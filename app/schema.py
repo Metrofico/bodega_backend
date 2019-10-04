387931034bb5a56261058df765debd5321de3230
Metrofico <joy_12lino@hotmail.com>
2019-10-04 17:57:28 -0500

    Continue Project

--- app/schema.py
+++ app/schema.py
@@ -4,6 +4,11 @@ import graphene
 class Mutation(graphene.AbstractType):
     register = graphene.String()
 
+    def resolve_register(self, info, **kwargs):
+        return "registrado"
+
+    pass
+
 
 class Query(graphene.AbstractType):
     user = graphene.String()
