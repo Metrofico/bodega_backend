ea9d7d686f4685d088709684657add5de9f98ad3
Brandon Zambrano <zambrano-brandon2621@unesum.edu.ec>
2019-10-07 09:26:36 -0500

    Model field update for conventions

--- app_usbodega/models.py
+++ app_usbodega/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Usuario(models.Model):
     id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=200)
-    apellido = models.CharField(max_length=200)
+    nombrea = models.CharField(max_length=200)
+    apellidos = models.CharField(max_length=200)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=1024)
