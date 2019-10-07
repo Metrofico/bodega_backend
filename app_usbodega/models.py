4531a17cee9b24f3aaa7aaf0e731690f7261d386
Brandon Zambrano <zambrano-brandon2621@unesum.edu.ec>
2019-10-07 09:27:29 -0500

    Model field update for conventions 2

--- app_usbodega/models.py
+++ app_usbodega/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Usuario(models.Model):
     id = models.AutoField(primary_key=True)
-    nombrea = models.CharField(max_length=200)
+    nombres = models.CharField(max_length=200)
     apellidos = models.CharField(max_length=200)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=1024)
--- bodega_backend/settings.py
+++ bodega_backend/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'bodega_unesum',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '5432',
     }
