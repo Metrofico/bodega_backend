b1caafbdb65546d57e9ee6d7fca39cd220de2a18
Metrofico <joy_12lino@outlook.com>
2019-10-22 23:04:14 -0500

    All Fixed repository + Update proyect no load promise Graphql-core / graphql-relay (Bug error)

--- app_usbodega/Mutations.py
+++ app_usbodega/Mutations.py
@@ -2,14 +2,12 @@ import graphene
 
 from app_usbodega.Catalogo import Catalogo
 from .Categoria import Categoria
-from .Medida import Medida
 from .Register import Register, Logout, AddProducto
 
 
 class Mutation(graphene.ObjectType):
     register = Register.Field()
     addproducto = AddProducto.Field()
-    addmedida = Medida.Field()
     addcategoria = Categoria.Field()
     addcatalogo = Catalogo.Field()
     logout = Logout.Field()
